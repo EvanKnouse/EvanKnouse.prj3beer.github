ab1133527af00d90fd78d3f4eb583e7b30481278
Joel Sipes <sipes5727@saskpolytech.ca>
2019-10-08 14:51:47 -0600

    Initial Master Commit -Joel Sipes

---
+++ prj3beer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using prj3beer.Models;
+
+namespace prj3beer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
