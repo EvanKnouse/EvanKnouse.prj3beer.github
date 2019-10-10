330bf2460c664a2c27c0f906b143e3ecfdc76779
Joel Sipes <sipes5727@saskpolytech.ca>
2019-10-10 14:02:27 -0600

    Created Test Folder Stucture, and Unit Test Skeleton, Ready to start coding unit tests - Joel Sipes

---
+++ nUnitTests/story06UserViewsCurrentBeverageTemperature.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace nUnitTests
+{
+    [TestFixture]
+    class story06UserViewsCurrentBeverageTemperature
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+
+        [TearDown]
+        public void Dispose() { }
+
+        [Test]
+        public void TestThatTemperatureIsBelowRange()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureIsAtMinimum()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureBelowZero()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureIsZero()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureIsAboveZero()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureIsAtMaximum()
+        {
+
+        }
+
+        [Test]
+        public void TestThatTemperatureIsAboveMaximum()
+        {
+
+        }
+
+        [Test]
+        public void TestThatDeviceIsDisconnected()
+        {
+
+        }
+
+        [Test]
+        public void TestThatDeviceIsDisconnectedForExtendedPeriod()
+        {
+
+        }
+
+        [Test]
+        public void TestThatDeviceSendsErrantData()
+        {
+
+        }
+
+        [Test]
+        public void TestThatDeviceIsContinuouslySendsErrantData()
+        {
+
+        }
+
+    }
+}
