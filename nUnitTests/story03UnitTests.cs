e6ac571526140b79e6b2ad89a493d3fd03626ec6
Evan Knouse <knouse8635@saskpolytech.ca>
2019-10-17 13:22:31 -0600

    Committing the addition of nUnitUI Tests to the project. - Evan and Carson

--- nUnitTests/story03UnitTests.cs
+++ nUnitTests/story03UnitTests.cs
@@ -7,28 +7,49 @@ namespace nUnitTests
 {
     class story03UnitTests
     {
+        MockBeverage mockBev;
+        int upperThreshold;
+        int lowerThreshold;
+        String error;
+
+
+        //In place of "null", -800 will be used for ints
+        int iNull = -800;
+
         [SetUp]
         public void Setup()
         {
             //Create a mock Beverage object to test with
-            MockBeverage mockBev = new MockBeverage("Banquet","Coors", -2);
+            mockBev = new MockBeverage("Banquet","Coors", -2);
 
             //Verify the properties of the mock beverage
             Assert.IsTrue(mockBev.idealTemp == -2);
             Assert.IsTrue(mockBev.name.Equals("Banquet"));
             Assert.IsTrue(mockBev.brand.Equals("Coors"));
+
+            //Set upper threshold
+            upperThreshold = 20;
+
+            //Set lower threshold
+            lowerThreshold = -10;
+
+            //Set default error message;
+            error = " ";
+
         }
 
         [Test]
         public void testTargetTemperatureIsDisplayed()
         {
-            
+
+            //Assert.IsTrue(textBox != null);
         }
 
         [Test]
         public void testBeverageTemperatureIsNull()
         {
-
+            Assert.IsTrue(mockBev.idealTemp == iNull);
+            Assert.IsTrue(error.Equals("Ideal temperature is not available"));
         }
 
         [Test]
@@ -44,15 +65,17 @@ namespace nUnitTests
         }
 
         [Test]
-        public void testIdealTempIsBelowUpperThreshold()
+        public void testIdealTempIsAboveUpperThreshold()
         {
-
+            Assert.IsTrue(mockBev.idealTemp > upperThreshold);
+            Assert.IsTrue(error.Equals("Ideal temperature is out of range."));
         }
 
         [Test]
-        public void testIdealTempisAboveLowerThreshold()
+        public void testIdealTempisBelowLowerThreshold()
         {
-
+            Assert.IsTrue(mockBev.idealTemp < lowerThreshold);
+            Assert.IsTrue(error.Equals("Ideal temperature is out of range."));
         }
     }
 }
