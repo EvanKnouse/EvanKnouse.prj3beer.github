9e250c636d0a4751ef8525af6c57c00de61cbc31
Anthony Smith <smith2619@saskpolytech.ca>
2019-10-22 14:56:55 -0600

    Began writing code stubs for the classes and mocks for story 6 --Anthony Smith & Joel Sipes

---
+++ prj3beer/Services/DeviceConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prj3beer.Services
+{
+    class DeviceConnection
+    {
+    }
+}
---
+++ prj3beer/Services/Temperature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prj3beer.Services
+{
+    class Temperature
+    {
+
+    }
+}
