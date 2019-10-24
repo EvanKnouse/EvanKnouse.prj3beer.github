3af71a6d1dac04a3709c9fea604de9c709ad271a
Evan Knouse <knouse8635@saskpolytech.ca>
2019-10-24 15:43:23 -0600

    Committing changes made to unit and ui tests to reflect the creation of the proper Beverage class. Evan and Carson

--- nUnitTests/story03UnitTests.cs
+++ nUnitTests/story03UnitTests.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+
 
 namespace nUnitTests
 {
     class story03UnitTests
     {
-        MockBeverage mockBev;
-        int upperThreshold;
-        int lowerThreshold;
+        Beverage mockBev;
         int iNull;
 
         [SetUp]
         public void Setup()
         {
             //Create a mock Beverage object to test with
-            mockBev = new MockBeverage("Banquet","Coors", -2);
-
-            //Set upper threshold
-            upperThreshold = 30;
-
-            //Set lower threshold
-            lowerThreshold = -30;
+            mockBev = new Beverage("Banquet","Coors", -2);
 
             // set mock null for integers
             iNull = -800;
@@ -40,7 +30,7 @@ namespace nUnitTests
         public void testAppCanGetBeverageIdealTemp()
         {
             // beverage is created with data from database, idealTemp is the expected value
-            mockBev = new MockBeverage("Banquet", "Coors", 2);
+            mockBev = new Beverage("Banquet", "Coors", 2);
 
             Assert.IsTrue(mockBev.getIdealTemp() == 2);
         }
@@ -49,7 +39,7 @@ namespace nUnitTests
         public void testBeverageTemperatureIsNull()
         {
             // beverage is created with data from database, idealTemp is null from database, mocked with -800
-            mockBev = new MockBeverage("Banquet", "Coors", iNull);
+            mockBev = new Beverage("Banquet", "Coors", iNull);
 
             Assert.IsTrue(mockBev.getIdealTemp() == 4);
         }
@@ -58,7 +48,7 @@ namespace nUnitTests
         public void testIdealTempIsAboveUpperThreshold()
         {
             // beverage is created with data from database, idealTemp is above upper threshold of 30
-            mockBev = new MockBeverage("Banquet", "Coors", 31);
+            mockBev = new Beverage("Banquet", "Coors", 31);
 
             Assert.IsTrue(mockBev.getIdealTemp() == 4);
         }
@@ -67,7 +57,7 @@ namespace nUnitTests
         public void testIdealTempisBelowLowerThreshold()
         {
             // beverage is created with data from database, idealTemp is below lower threshold of -30
-            mockBev = new MockBeverage("Banquet", "Coors", -31);
+            mockBev = new Beverage("Banquet", "Coors", -31);
 
             Assert.IsTrue(mockBev.getIdealTemp() == 4);
         }
--- prj3beer/Services/Beverage.cs
+++ prj3beer/Services/Beverage.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace nUnitTests
+﻿namespace prj3beer.Services
 {
-    public class MockBeverage
+    public class Beverage
     {
         public String name;
         public String brand;
         private int idealTemp;
         public int faveTemp;
         private const int defaultTemp = 4;
-        
 
-        public MockBeverage(String sName, String sBrand, int nIdealTemp)
+        //Default Beverage Constructor
+        public Beverage()
+        {
+
+        }
+
+        public Beverage(String sName, String sBrand, int nIdealTemp)
         {
             this.name = sName;
             this.brand = sBrand;
+            this.idealTemp = nIdealTemp;
 
             //If the idealTemp is set outside the allowed range, set it to default value
-            if(nIdealTemp > 30 || nIdealTemp < -30)
+            if (nIdealTemp > 30 || nIdealTemp < -30)
             {
                 this.idealTemp = defaultTemp;
             }
