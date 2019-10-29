e150a918eb12644fa18e371100ecab2c2005bdd7
Anthony Smith <smith2619@saskpolytech.ca>
2019-10-29 14:58:57 -0600

    Created Android-only project as per Ernesto/Wade's suggestion

---
+++ prj3beerAndroid/prj3beerAndroid/BlueToothDevice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Bluetooth;
+
+namespace prj3beerAndroid
+{
+    class BlueToothDevice : I_Reader
+    {
+        string hexTemp;
+        BluetoothAdapter btDevice;
+
+        public int GetTemp()
+        {
+            return ConvertHex(this.hexTemp);
+        }
+
+        private int ConvertHex(string hexTemp)
+        {
+            //Converts raw hex value into integer representing degrees celsius
+            return 0;
+        }
+
+        private BluetoothAdapter 
+    }
+}
---
+++ prj3beerAndroid/prj3beerAndroid/MockDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace prj3beerAndroid
+{
+    class MockDevice : I_Reader
+    {
+        int[] dummyValues;
+        int currentValue;
+
+        public MockDevice(params int[] values)
+        {
+            this.dummyValues = values;
+            this.currentValue = -1;
+        }
+
+        public int GetTemp()
+        {
+            return dummyValues[++currentValue];
+        }
+    }
+}
