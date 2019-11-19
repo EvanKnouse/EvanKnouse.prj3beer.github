87e3e9aca71be9f6211f4d0d44956624214a8eba
Simon Gilmour <cst210@CST.SIAST.SK.CA>
2019-11-19 14:56:27 -0600

    Started Scaffolding Brand Class and the BrandContext class for setting up the database. Started getting the Entity Framework up and running. P: Simon CP: Joel

---
+++ prj3beer/Models/Brand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace prj3beer.Models
+{
+ 
+    public class Brand
+    {
+        [Key]
+        public int brandID { get; set; }
+        public String brandName { get; set; }
+
+        public Brand()
+        {
+
+        }
+
+
+    }
+
+   
+
+}
---
+++ prj3beer/Models/BrandContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace prj3beer.Models
+{
+    public class BrandContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        private string _databasePath { get; set; }
+
+        public BrandContext()
+        {
+
+        }
+
+        public BrandContext(string databasePath)
+        {
+            _databasePath = databasePath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={_databasePath}");
+        }
+    }
+}
