3b73a6f8e4f5778f6cc3b475b8e77d651fad61bb
Evan Knouse <knouse8635@saskpolytech.ca>
2019-11-21 14:05:52 -0600

    Committing work done on UI tests. Added multiple tests. As well, configured the development environment to enable UI Testing, and committing these changes. Evan and Anthony

--- UITests/Story01UserViewsAndInteractsWithSettingsMenuUITests.cs
+++ UITests/Story01UserViewsAndInteractsWithSettingsMenuUITests.cs
@@ -22,16 +22,70 @@ namespace UITests
         [SetUp]
         public void BeforeEachTest()
         {
-            app = AppInitializer.StartApp(platform);
+            //Initialize the app, arrive at home page (default for now)
+            app = ConfigureApp.Android.ApkFile(@"D:\virpc\prj3beer\prj3.beer\prj3beer.Android\bin\Debug\com.companyname.prj3beer.apk").StartApp();
+            //Tap into the screen navigation menu (default for now)
+            app.Tap(c=>c.Button("ScreenSelectButton"));
         }
 
         [Test]
-        public void WelcomeTextIsDisplayed()
+        public void TestTemperatureBelowRangeError()
         {
-            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
-            app.Screenshot("Welcome screen.");
+            AppResult[] results = app.WaitForElement(c => c.Marked("CurrentTempBox"));
+            app.Screenshot("Status screen.");
 
-            Assert.IsTrue(results.Any());
+            Assert.Equals(results[0].Text, "Temperature reading outside of range");
+        }
+
+        [Test]
+        public void TestTemperatureAboveRangeError()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
+            app.Screenshot("Status screen.");
+
+            Assert.Equals(results[0].Text, "Temperature reading outside of range");
+        }
+
+        [Test]
+        public void TestDeviceNotFoundError()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
+            app.Screenshot("Status screen.");
+
+            Assert.Equals(results[0].Text, "Waiting for device");
+        }
+
+        [Test]
+        public void TestTemperatureInRangeAtMinusOne()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
+            app.Screenshot("Status screen.");
+            Assert.Equals(results[0].Text, "-1\u00B0C");
+        }
+
+        [Test]
+        public void TestSettingsMenuIsDisplayedOnStatusScreenWhenSettingsButtonIsPressed()
+        {
+            app.Tap(c => c.Button("SettingsMenuButton"));
+        }
+
+        [Test]
+        public void TestSettingsMenuIsDisplayedOnSelectionScreenWhenSettingsButtonIsPressed()
+        {
+            app.Tap(c => c.Button("SettingsMenuButton"));
+        }
+
+        [Test]
+        public void TestSettingsAreAppliedOnSettingsChange()
+        {
+            app.Tap(c => c.Button("SettingsMenuButton"));
+            app.Tap(c => c.Button("TemperatureSettingsButton"));
+        }
+
+        [Test]
+        public void TestThatSettingsMenuDisplaysCurrentAppSettings()
+        {
+
         }
     }
 }
