ba4932b805b89cfa2f91f81dc8a0e8c82906f632
Evan Knouse <knouse8635@saskpolytech.ca>
2019-11-21 16:19:48 -0600

    Committing more UI tests. Evan and Anthony

--- UITests/Story01UserViewsAndInteractsWithSettingsMenuUITests.cs
+++ UITests/Story01UserViewsAndInteractsWithSettingsMenuUITests.cs
@@ -25,14 +25,14 @@ namespace UITests
             //Initialize the app, arrive at home page (default for now)
             app = ConfigureApp.Android.ApkFile(@"D:\virpc\prj3beer\prj3.beer\prj3beer.Android\bin\Debug\com.companyname.prj3beer.apk").StartApp();
             //Tap into the screen navigation menu (default for now)
-            app.Tap(c=>c.Button("ScreenSelectButton"));
+            app.Tap("ScreenSelectButton");
         }
 
         [Test]
         public void TestTemperatureBelowRangeError()
         {
             AppResult[] results = app.WaitForElement(c => c.Marked("CurrentTempBox"));
-            app.Screenshot("Status screen.");
+            app.Screenshot("Status");
 
             Assert.Equals(results[0].Text, "Temperature reading outside of range");
         }
@@ -41,7 +41,7 @@ namespace UITests
         public void TestTemperatureAboveRangeError()
         {
             AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
-            app.Screenshot("Status screen.");
+            app.Screenshot("Status");
 
             Assert.Equals(results[0].Text, "Temperature reading outside of range");
         }
@@ -50,7 +50,7 @@ namespace UITests
         public void TestDeviceNotFoundError()
         {
             AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
-            app.Screenshot("Status screen.");
+            app.Screenshot("Status");
 
             Assert.Equals(results[0].Text, "Waiting for device");
         }
@@ -59,33 +59,67 @@ namespace UITests
         public void TestTemperatureInRangeAtMinusOne()
         {
             AppResult[] results = app.WaitForElement(c => c.Marked("currentTemp"));
-            app.Screenshot("Status screen.");
+            app.Screenshot("Status");
             Assert.Equals(results[0].Text, "-1\u00B0C");
         }
 
         [Test]
         public void TestSettingsMenuIsDisplayedOnStatusScreenWhenSettingsButtonIsPressed()
         {
-            app.Tap(c => c.Button("SettingsMenuButton"));
+            //Pick Status screen from the screen selection menu
+            app.Tap("Status");
+            //Press Settings Menu button
+            app.Tap("SettingsMenuButton");
+            //Look for the toggle button on the Settings Menu
+            AppResult[] button = app.Query(("temperatureToggle"));
+            //Will be greater than 0 if it exists, returns AppResult[]
+            Assert.IsTrue(button.Any());
         }
 
         [Test]
         public void TestSettingsMenuIsDisplayedOnSelectionScreenWhenSettingsButtonIsPressed()
         {
-            app.Tap(c => c.Button("SettingsMenuButton"));
+            //Pick Status screen from the screen selection menu
+            app.Tap("Selection");
+            //Press Settings Menu button
+            app.Tap("SettingsMenuButton");
+            //Look for the toggle button on the Settings Menu
+            AppResult[] button = app.Query(("temperatureToggle"));
+            //Will be greater than 0 if it exists, returns AppResult[]
+            Assert.IsTrue(button.Any());
         }
 
         [Test]
         public void TestSettingsAreAppliedOnSettingsChange()
         {
-            app.Tap(c => c.Button("SettingsMenuButton"));
-            app.Tap(c => c.Button("TemperatureSettingsButton"));
+            //Pick Status screen from the screen selection menu
+            app.Tap("Status");
+            //Press Settings menu button
+            app.Tap("SettingsMenuButton");
+            //Tap on the toggle button to change the temperature setting to fahrenheit
+            app.Tap("temperatureToggle");
+            //Go back to the Status screen
+            app.Back();
+            //Check that the label for the current temperature is set to "\u00B0F"
+            string tempLabel = app.Query("currentTempLabel")[0].Text;
+
+            Assert.AreEqual("\u00B0F", tempLabel);
         }
 
         [Test]
         public void TestThatSettingsMenuDisplaysCurrentAppSettings()
         {
-
+            //Pick status screen from the screen selection menu
+            app.Tap("Status");
+            //Press Settings menu button
+            app.Tap("SettingsMenuButton");
+            //Tap on the toggle button to change the temperature setting to fahrenheit 
+            app.Tap("temperatureToggle");
+            //Go back to the Status screen
+            app.Back();
+            //Go back to the Settings menu button
+            app.Tap("SettingsMenuButton");
+            bool isToggled = app.Query((Xamarin.Forms Switch)"temperatureToggle");
         }
     }
 }
