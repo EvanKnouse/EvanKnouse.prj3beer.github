5a2218ce59d77566ec454fcaf6ad3d83a3d6ed18
Aaron Atkinson <atkinson9130@saskpolytech.ca>
2019-11-28 13:51:57 -0600

    Changed +/- buttons to a stepper, added methods to load up the beverage on start up, left comments on the status.ca file to guide new group member with the logical flow of our story -Aaron

--- prj3beer/prj3beer/Views/status.xaml.cs
+++ prj3beer/prj3beer/Views/status.xaml.cs
@@ -1,4 +1,5 @@
-﻿using prj3beer.Services;
+﻿using prj3beer.Models;
+using prj3beer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,16 +17,59 @@ namespace prj3beer.Views
         public status()
         {
             InitializeComponent();
+
+            /**
+             * Get the beverage
+              *     Check if an preference already exists for the beverage
+              *         if it does display the perferences target temp
+              *         else display the beverages target temp
+              *  
+              */
+            Beverage currentBeverage = getBeverageFromLocalStorage();
+            setTargetTempEntryOnStartUp(currentBeverage);
+
         }
 
-        private void BtnIncTemp_Clicked(object sender, EventArgs e)
+        private void setTargetTempEntryOnStartUp(Beverage currentBeverage)
         {
 
+            //Should check to see if a preference exists for the currentbeverage
+
+            //Sets the currenTarget text to the currentbeverage's ideal temp
+            currentTarget.Text = currentBeverage.IdealTemp.ToString();
         }
 
-        private void BtnDecTemp_Clicked(object sender, EventArgs e)
+        private Beverage getBeverageFromLocalStorage()
         {
+            //Right now We dont have local storage working, for now send back a Beverage object
 
+            Beverage mockBev = new Beverage(1, "MGD", "Miller", 2);
+
+            return mockBev;
+        }
+
+        private void StpTemp_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            /**
+             * "ideal" implementation
+             * 
+             * Whenever the stepper value is changed by the steppers or typed in...
+             * set the currentTarget Entry to +/- its previous value
+             *      Check for a preference for that beverage
+             *          if it exists
+             *              update its temperature value
+             *          else
+             *              Create a new one with the agjusted target temperature
+             *       Save it locally
+             *       
+             * note: right now the typed in values dont register,
+             *       maybe on Entry change call this even handler with the new value passed in
+             * 
+             */
+
+
+
+            currentTarget.Text = e.NewValue.ToString();
         }
     }
 }
