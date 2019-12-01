7f5c6f83a11c5a22880f25d3c6c8dfea15db35f0
Carson Kearns <kearns7176@saskpolytech.ca>
2019-12-01 15:05:11 -0600

    Continued working on status page methods for incrementing and decrementing the beverage's current temperature. Added checks for if the application is in celsius or fahrenheit mode. P: Carson CP: Joel

--- prj3beer/prj3beer/Views/status.xaml.cs
+++ prj3beer/prj3beer/Views/status.xaml.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,18 +13,17 @@ namespace prj3beer.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Status : ContentPage
     {
+        // beverage object, set from database selection or favourites selection
         Beverage currentBeverage;
-        
-        Temperature currentTemp = new Temperature();
+        bool isCelsius = true;
+        //Temperature currentTemp = new Temperature();
        
         public Status()
         {
             InitializeComponent();
 
-            currentTemp.isCelsius = false;
-
-            
-
+            // set the current temperature setting to fahrenheit for testing purposes
+            //currentTemp.isCelsius = false;
 
             /**
              * Get the beverage
@@ -34,61 +32,103 @@ namespace prj3beer.Views
               *         else display the beverages target temp
               *  
               */
-            currentBeverage = getBeverageFromLocalStorage();
-            setTargetTempEntryOnStartUp(currentBeverage);
+            // set the currentBeverage attribute to a beverage object returned from a database (eventually)
+            currentBeverage = GetBeverageFromLocalStorage();
+
+            // 
+            SetTargetTempEntryOnStartUp(currentBeverage);
 
             stpTemp.Value = currentBeverage.IdealTemp;
 
-            updateSteppers();
-            //StpTemp_ValueChanged(currentBeverage, null);
+            UpdateSteppers();
 
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
         }
 
-        private void updateSteppers()
+        private Beverage GetBeverageFromLocalStorage()
         {
-            if (currentTemp.isCelsius)
-            {
-                stpTemp.Minimum = -30;
-                stpTemp.Maximum =  30;
-
-            }
-            else
-            {
-                stpTemp.Minimum = currentTemp.CelsiusToFahrenheit(-30);
-                stpTemp.Maximum = currentTemp.CelsiusToFahrenheit( 30);
-                stpTemp.Value = Math.Round(currentBeverage.IdealTemp * 1.8 + 32);
-            }
-            
-        }
-
-        private void setTargetTempEntryOnStartUp(Beverage currentBeverage)
-        {
-            //Should check to see if a preference exists for the currentbeverage
-
-            //Sets the currenTarget text to the currentbeverage's ideal temp
-            double temp = currentBeverage.IdealTemp;
-
-            currentTarget.Text = temp.ToString();
-        }
-
-        private Beverage getBeverageFromLocalStorage()
-        {
-            //Right now We dont have local storage working, for now send back a Beverage object
-
+            // right now we don't have local storage working, so we'll send a hardcoded beverage 
             Beverage mockBev = new Beverage();
+            // hardcoded values, will eventually get these values from a table
             mockBev.BevID = 1;
             mockBev.Name = "Miller";
             mockBev.IdealTemp = 2;
 
+            // return the beverage, to be set to the status page's currentBeverage attribute
             return mockBev;
+        }
+
+        private void SetTargetTempEntryOnStartUp(Beverage currentBeverage)
+        {
+            // sets the currenTarget text to the currentBeverage's ideal temp
+            // the ideal temperature of the beverage object is set from a value either in the main beverage table or favourites table
+            double temp = currentBeverage.IdealTemp;
+
+            // set the entry field text value to the currentBeverage's ideal temperature
+            currentTarget.Text = temp.ToString();
+
+            // debug purposes
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
+        }
+
+        private void UpdateSteppers()
+        {
+            // set the minimum and maximum values for the stepper, whether
+            double currentTemp = Math.Round(currentBeverage.IdealTemp); // 2
+            double minValue = -30;
+            double maxValue = 30;
+
+            if(!isCelsius)
+            {
+                minValue = -30 * 1.8 + 32; // -22
+                maxValue = 30 * 1.8 + 32; // 86
+                currentTemp = Math.Round(currentTemp * 1.8 + 32); // 36
+                //stpTemp.Value = Math.Round(currentBeverage.IdealTemp);
+            }
+            /*else
+            {
+                //currentTemp = currentBeverage.IdealTemp;
+                stpTemp.Minimum = -30 * 1.8 + 32;
+                stpTemp.Maximum = 30 * 1.8 + 32;
+                stpTemp.Value = Math.Round(currentBeverage.IdealTemp * 1.8 + 32);
+            }*/
+
+            stpTemp.Minimum = minValue;
+            stpTemp.Maximum = maxValue;
+            stpTemp.Value = currentTemp;
+
+            // debug purposes
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
         }
 
         private void RegisterTemperatureChange(object sender, EventArgs e)
         {
-            double newVal = Convert.ToDouble(((Entry)sender).Text);
+            double newVal;
 
-            currentBeverage.IdealTemp = newVal;
-            stpTemp.Value = currentBeverage.IdealTemp;
+            if (string.IsNullOrEmpty(((Entry)sender).Text))
+            {
+                newVal = isCelsius ? currentBeverage.IdealTemp : ((currentBeverage.IdealTemp * 1.8) + 32);
+                ((Entry)sender).Text = newVal.ToString();
+            }
+            else
+            {
+                newVal = Convert.ToDouble(((Entry)sender).Text);
+            }
+
+            /*if(isCelsius)
+            {
+                currentBeverage.IdealTemp = newVal;
+            }
+            else if(!isCelsius)
+            {
+                currentBeverage.IdealTemp = (newVal - 32) * 5 / 9;
+            }*/
+
+            //stpTemp.Value = currentBeverage.IdealTemp;
+            stpTemp.Value = newVal;
+
+            // debug purposes
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
         }
 
         private void StpTemp_ValueChanged(object sender, ValueChangedEventArgs e)
@@ -110,9 +150,35 @@ namespace prj3beer.Views
              * 
              */
 
-            currentBeverage.IdealTemp = e.NewValue;
+            double newTemp = e.NewValue;
 
+            if(isCelsius)
+            {
+                currentBeverage.IdealTemp = e.NewValue;
+                //newTemp = ( - 32)
+            }
+            else
+            {
+                currentBeverage.IdealTemp = (e.NewValue - 32) * 5 / 9;
+            }
+            
             currentTarget.Text = Math.Round(e.NewValue).ToString();
+
+            // debug purposes
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
+        }
+
+        private void ChangeTempMetric(object sender, EventArgs args)
+        {
+            isCelsius = isCelsius == true ? false : true;
+
+            // debug purposes
+            currentMetric.Text = isCelsius ? "Celsius" : "Fahrenheit";
+
+            UpdateSteppers();
+
+            // debug purposes
+            currentIdealTemp.Text = currentBeverage.IdealTemp.ToString();
         }
     }
 }
