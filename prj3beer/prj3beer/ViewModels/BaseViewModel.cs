8d7566fac7be703fd524a585a7ac27008921e32d
Joel Sipes <sipes5727@saskpolytech.ca>
2020-01-28 13:52:46 -0600

    Removed BasePageViewModel + BrandPageViewModel's, as they are obsolete. Added new BasePageViewModel, CredentialSelectViewModel and MainPageViewModel - Joel & Carson

---
+++ prj3beer/prj3beer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace prj3beer.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName]string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            {
+                return false;
+            }
+            backingStore = value;
+            onChanged?.Invoke();
+            onPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void onPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler changed = PropertyChanged;
+            if(changed == null)
+            {
+                return;
+            }
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
