80c94de54c92d86c694dee0687ac933416274194
Evan Knouse <knouse8635@saskpolytech.ca>
2020-02-03 14:56:28 -0600

    Adding functionality to allow the notification sub settings to appear on toggle of the master switch. Added conditional logic to the notifications (show on toggle). Evan and Anthony

---
+++ prj3beer/prj3beer/Services/TargetTempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace prj3beer.Services
+{
+    class TargetTempConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double temp = (double)value; //The temperature is passed-in as an object, cast it back to a double
+
+            //Checks if the fahrenheit setting is applied and addends the appropriate units to the unconverted/converted temp
+            return Models.Settings.TemperatureSettings ? (double)temp : Temperature.CelsiusToFahrenheit((double)temp);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double temp = double.Parse((string)value);
+
+            return Models.Settings.TemperatureSettings ? temp : Temperature.FahrenheitToCelsius(temp);
+
+        }
+    }
+}
--- prj3beer/prj3beer/Views/SettingsMenu.xaml.cs
+++ prj3beer/prj3beer/Views/SettingsMenu.xaml.cs
@@ -7,22 +7,66 @@ namespace prj3beer.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsMenu : ContentPage
-    {   
+    {
+        //Initialize a container for the notifications sub section
+        TableSection subSection;
+
+        //
+        SwitchCell inRangeNotifications;
+
+        //
+        SwitchCell tooHotColdNotifications;
+
         public SettingsMenu()
         {
             InitializeComponent();
+
+            //Create the in range notifications switch cell
+            //Create an event handler to handle the toggling of the switch
+            inRangeNotifications = new SwitchCell { Text = "In Range" };
+            inRangeNotifications.OnChanged += (sender, e) =>
+            {
+                Models.Settings.InRangeSettings = e.Value;
+            };
+
+            //Create the in range notifications switch cell
+            //Create an event handler to handle the toggling of the switch
+            tooHotColdNotifications = new SwitchCell { Text = "Too Hot/Too Cold" };
+            tooHotColdNotifications.OnChanged += (sender, e) =>
+            {
+                Models.Settings.TooHotColdSettings = e.Value;
+            };
+
+            //Instantiate the container
+            subSection = new TableSection();
+            subSection.Add(inRangeNotifications);
+            subSection.Add(tooHotColdNotifications);
+
+            //Set the notification master switch to on by default
+            switchNotifications.On = Models.Settings.NotificationSettings;
+
+            //If the master notification switch is set to true,
+            //if (Models.Settings.NotificationSettings)
+            //{
+            //    //Add the notification sub section to the settings table
+            //    ShowSubSettings();
+            //}
 
             //Sets the Label on the switch to match the current temperature setting.
             switchTemp.Text = Models.Settings.TemperatureSettings ? "Celsius" : "Fahrenheit";
 
             //Check the Settings class to see if set to celsius or fahrenheit
             switchTemp.On = Models.Settings.TemperatureSettings;
-
-            //Set the notification master switch to on by default
-            switchNotifications.On = Models.Settings.NotificationSettings;
         }
 
-        //changes temperature display settings in response to the switch 
+
+
+        /// <summary>
+        /// This method will handle the temperature switch functionality
+        /// Allows the app to be changed between displaying in Celsius or Fahrenheit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Temp_Switch_Toggled(object sender, ToggledEventArgs e)
         {
             switchTemp.Text = e.Value ? "Celsius" : "Fahrenheit";
@@ -30,19 +74,48 @@ namespace prj3beer.Views
             Models.Settings.TemperatureSettings = e.Value;
         }
 
+        /// <summary>
+        /// This method will handle the notification master switch functionality
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Notifications_Switch_Toggled(object sender, ToggledEventArgs e)
         {
+            //Set the master notification setting to the switch's value (true or false)
             Models.Settings.NotificationSettings = e.Value;
+
+            if (e.Value)
+            {
+                ShowSubSettings();
+            }
+            else
+            {
+                HideSubSettings();
+            }
         }
 
-        private void InRange_Switch_Toggled(object sender, ToggledEventArgs e)
+        private void HideSubSettings()
         {
-            Models.Settings.InRangeSettings = e.Value;
+            //Disable the in range notifications
+            inRangeNotifications.IsEnabled = false;
+
+            //Disable the too hot/ too cold notifications
+            tooHotColdNotifications.IsEnabled = false;
+
+            //Remove the notification sub section from the settings table view
+            SettingsTable.Root.Remove(subSection);
         }
 
-        private void TooHotCold_Switch_Toggled(object sender, ToggledEventArgs e)
+        private void ShowSubSettings()
         {
-            Models.Settings.TooHotColdSettings = e.Value;
+            //Add the notification subsection 
+            SettingsTable.Root.Add(subSection);
+
+            //Enable the in range notifications
+            inRangeNotifications.IsEnabled = true;
+
+            //Enable the too hot/ too cold notifications
+            tooHotColdNotifications.IsEnabled = true;
         }
 
         //Closes the settings modal
